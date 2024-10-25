1c8fa39382c77855e21ee15d98aada05df9efc8f
Mark Peters <mapeters101@earthlink.net>
2024-10-25 17:15:19 -0400

    Lesson09:  Complete lesson.

--- training.py
+++ training.py
@@ -1,13 +1,15 @@
 import os
 from canvas import Canvas
 from racetrack import Track
-from network import FirstNetwork
+from network import Network
 
 car_image_paths = [os.path.join("images", f"car{i}.png") for i in range(5)]
 canvas = Canvas(Track(0), car_image_paths)
 
+# Network and genetic algorithm configuration
+network_dimensions = 5, 4, 2  # input neurons, hidden layer neurons, output neurons
 population_count = 3
-networks = [FirstNetwork() for _ in range(population_count)]
+networks = [Network(network_dimensions) for _ in range(population_count)]
 
 simulation_round = 1
 canvas.simulate_generation(networks, simulation_round)
