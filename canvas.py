59b686458a8cf13bf81cf2c3e81dc88a01e1b35d
Mark Peters <mapeters101@earthlink.net>
2024-10-25 10:35:32 -0400

    Lesson05:  Complete lesson from 3:10. FINAL version of manual controlled car!!

--- canvas.py
+++ canvas.py
@@ -9,14 +9,15 @@ from car import Car
 
 class Canvas(Window):
     frame_duration = 1 / 60
-    def __init__(self, track_image_path, car_image_paths):
+    def __init__(self, track, car_image_paths):
         super().__init__()
+        self.track = track
         self.is_simulating = True
         self.width = 960
         self.height = 540
         self.background_batch = Batch()
         self.cars_batch = Batch()
-        self.track_image_sprite = Sprite(image.load(track_image_path), batch=self.background_batch)
+        self.track_image_sprite = Sprite(track.track_image, batch=self.background_batch)
         self.car_images = [image.load(c) for c in car_image_paths]
         self.keyboard = key.KeyStateHandler()  # TODO: remove keyboard control
         self.push_handlers(self.keyboard)  # TODO: remove keyboard control
@@ -38,6 +39,9 @@ class Canvas(Window):
     def update(self, delta_time):
         for car_sprite in self.car_sprites:
             car_sprite.update(delta_time, self.keyboard)   # TODO: remove keyboard control
+            if car_sprite.is_running:
+                if not self.track.is_road(car_sprite.body.x, car_sprite.body.y):
+                    car_sprite.shut_off()
 
     def draw(self):
         self.clear()
