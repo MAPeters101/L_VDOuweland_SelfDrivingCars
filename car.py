d20978a4638c77d66fa931d8a6e6fa4f0c3b87e6
Mark Peters <mapeters101@earthlink.net>
2024-10-25 10:50:51 -0400

    Lesson06:  Complete lesson.

--- car.py
+++ car.py
@@ -1,12 +1,11 @@
 import math
 from pyglet.sprite import Sprite
-from pyglet.window import key  # TODO: remove keyboard control
-
 
 class Car:
     max_speed = 6.0
 
-    def __init__(self, image, batch):
+    def __init__(self, network, image, batch):
+        self.network = network
         image.anchor_x = 25
         image.anchor_y = 25
         self.body = Sprite(image, batch=batch)
@@ -15,18 +14,11 @@ class Car:
         self.rotation = 0.0
         self.is_running = True
 
-    def update(self, delta_time, keyboard):   # TODO: remove keyboard control
+    def update(self, delta_time):
         render_speed = delta_time * 60
         self.speed -= 0.05  # friction
         if self.is_running:
-            acceleration = 0.0
-            steer_position = 0.0
-            if keyboard[key.UP]:
-                acceleration = 1.0
-            if keyboard[key.LEFT]:
-                steer_position = -1.0
-            elif keyboard[key.RIGHT]:
-                steer_position = 1.0
+            acceleration, steer_position = self.network.feed_forward()
 
             if acceleration > 0:
                 self.speed += 0.1
--- training.py
+++ training.py
@@ -1,10 +1,13 @@
 import os
 from canvas import Canvas
 from racetrack import Track
+from network import FirstNetwork
 
 car_image_paths = [os.path.join("images", f"car{i}.png") for i in range(5)]
 canvas = Canvas(Track(), car_image_paths)
 
-canvas.simulate_generation()
+population_count = 3
+networks = [FirstNetwork() for _ in range(population_count)]
+canvas.simulate_generation(networks)
 
 
