64935a79b09af0c513756fa819e4778f348655b5
Mark Peters <mapeters101@earthlink.net>
2024-10-25 16:13:08 -0400

    Lesson08:  Complete lesson.

--- training.py
+++ training.py
@@ -4,7 +4,7 @@ from racetrack import Track
 from network import FirstNetwork
 
 car_image_paths = [os.path.join("images", f"car{i}.png") for i in range(5)]
-canvas = Canvas(Track(), car_image_paths)
+canvas = Canvas(Track(0), car_image_paths)
 
 population_count = 3
 networks = [FirstNetwork() for _ in range(population_count)]
